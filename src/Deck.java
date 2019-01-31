708557df20580558a22bf6199f40e54d24bca523
RobynDalgleish <h0arifyed@gmail.com>
2019-01-30 20:20:37 -0500

    Initial commit

---
+++ src/Deck.java
@@ -0,0 +1,29 @@
+import java.util.Collections;
+import java.util.Optional;
+import java.util.Stack;
+
+public class Deck {
+    private final Stack<Card> deckOfCards;
+
+    private Deck() {
+        this.deckOfCards = initDeck();
+    }
+
+    private Stack<Card> initDeck() {
+        final Stack<Card> deckOfCards = new Stack<>();
+        for(final Card.Suit suit : Card.Suit.values()) {
+            for (final Card.Rank rank : Card.Rank.values()) {
+                deckOfCards.push(Card.getCard(rank, suit));
+            }
+        }
+        Collections.shuffle(deckOfCards);
+        return deckOfCards;
+    }
+
+    public Optional<Card> deal() {
+        return this.deckOfCards.empty() ? Optional.empty() :
+                Optional.of(this.deckOfCards.pop());
+    }
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.stream.IntStream;
+
+public class Main {
+
+    public static void main(String[] args) {
+        final Deck deck = new Deck();
+        final int numOfCardsToDeal = 52;
+        IntStream.range(0, numOfCardsToDeal);
+    }
+
+}
